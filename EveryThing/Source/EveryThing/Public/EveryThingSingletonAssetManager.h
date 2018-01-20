3642274c51185dedb226c39a552fd93e54afd846
HXSD-Basic\hxsd <1280753384@qq.com>
2018-01-20 21:03:14 +0800

    +.添加一个游戏资产管理类 +.更新项目需求

---
+++ EveryThing/Source/EveryThing/Public/EveryThingSingletonAssetManager.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "EveryThingSingletonAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVERYTHING_API UEveryThingSingletonAssetManager : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
